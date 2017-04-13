79ed68d1e0b8ff9a1e333ed4b161857539ef9896
Hüseyin Karakullukçu <karakullukcuhuseyin@gmail.com>
2017-04-13 22:48:16 +0300

    PhotoPreviewFragment added

---
+++ app/src/main/java/com/karakullukcu/huseyin/quickphoto/PhotoPreviewFragment.java
@@ -0,0 +1,25 @@
+package com.karakullukcu.huseyin.quickphoto;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PhotoPreviewFragment extends Fragment {
+
+
+    public PhotoPreviewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_photo_preview, container, false);
+    }
+
+}
