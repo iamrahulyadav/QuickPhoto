02d782964a8152559b9d87ccce50913936d46594
Hüseyin Karakullukçu <karakullukcuhuseyin@gmail.com>
2017-04-14 17:02:20 +0300

    make app fullscreen

--- app/src/main/java/com/karakullukcu/huseyin/quickphoto/MainActivity.java
+++ app/src/main/java/com/karakullukcu/huseyin/quickphoto/MainActivity.java
@@ -1,8 +1,12 @@
 package com.karakullukcu.huseyin.quickphoto;
 
+import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragmentContainerLayout,new CameraPreviewFragment());
-        ft.commit();
+
+        hideSystemUI();
+
+        // Transparent system ui after api 19.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+        final Handler handler = new Handler();
+        final Runnable hideUiTask = new Runnable() {
+            @Override
+            public void run() {
+                hideSystemUI();
+            }
+        };
+
+        // System ui disappear 3 seconds after shewed up.
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    handler.postDelayed(hideUiTask,3000);
+                } else {
+                    handler.removeCallbacks(hideUiTask);
+                }
+            }
+        });
+
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainerLayout, new CameraPreviewFragment());
+        transaction.commit();
+    }
+
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
